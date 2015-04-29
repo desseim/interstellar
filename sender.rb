a675561885c699ad0ff7cc3b0332643442a80c4a
Yanni Chang <ccyanni@gmail.com>
2015-04-29 11:07:33 +0800

    The "BOTO_PATH" env setting didn't work somehow. Use "system([env,] command... [,options])" and pass the [env] as a hash argument instead.

--- sender.rb
+++ sender.rb
@@ -13,7 +13,7 @@ date = Date.today-2
 file_date = date.strftime("%Y%m")
 csv_file_name = "reviews_#{CONFIG["package_name"]}_#{file_date}.csv"
 
-system "BOTO_PATH=./secrets/.boto gsutil/gsutil cp -r gs://#{CONFIG["app_repo"]}/reviews/#{csv_file_name} ."
+system({"BOTO_PATH"=>"./secrets/.boto"}, "gsutil/gsutil cp -r gs://#{CONFIG["app_repo"]}/reviews/#{csv_file_name} .")
 
 
 class Slack
